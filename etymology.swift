1ef421e3c6c728286f178af98e3623b5ded34449
Shen Yanbin <shendian1990@gmail.com>
2019-01-22 16:22:11 +0800

    确定cell显示方式

---
+++ etymology.swift
@@ -0,0 +1,13 @@
+class Etymology{
+    var name: String
+    var explain: String
+    var words: [Word]
+}
+
+class Word{
+    var name: String
+    var pronunciation: String
+    var defination: String
+    var explain: String
+    var examplSentence: String
+}
